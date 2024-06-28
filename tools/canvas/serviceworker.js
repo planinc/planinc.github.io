a4cdc13a687403d3f303864c97bd1563cc500427
planinc <mucinhasan9@gmail.com>
2024-06-28 21:26:18 +0700

    Add files via upload

---
+++ tools/canvas/serviceworker.js
@@ -0,0 +1,37 @@
+
+let cacheName = "static";
+let contentToCache = [
+  "/index.html",
+  "/js/storage.js",
+  "/js/draw.js",
+  "/js/index.js"
+]
+
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] Install");
+  e.waitUntil(
+    (async () => {
+      const cache = await caches.open(cacheName);
+      console.log("[Service Worker] Caching");
+      await cache.addAll(contentToCache);
+    })()
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    (async () => {
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (r) {
+        return r;
+      }
+      const response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })()
+  );
+});
+
