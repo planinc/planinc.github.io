4c8f65cfb3e39cbe228c755d6cd76b1a49efc173
planinc <mucinhasan9@gmail.com>
2024-07-03 20:31:19 +0700

    Create \Redist\CommonConfiguration\Neutral\SimpleMath\js\planinc.js

---
+++ \Redist\CommonConfiguration\Neutral\SimpleMath\js\planinc.js
@@ -0,0 +1,21 @@
+(function (global) {
+    "use strict";
+    global.planinc = {
+        add: function (firstNumber, secondNumber) {
+            return firstNumber + secondNumber;
+        },
+
+        subtract: function (firstNumber, secondNumber) {
+            return firstNumber - secondNumber;
+        },
+
+        multiply: function (firstNumber, secondNumber) {
+            return firstNumber * secondNumber;
+        },
+
+        divide: function (firstNumber, secondNumber) {
+            return firstNumber / secondNumber;
+        }
+    };
+})(this);
+
